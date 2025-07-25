bcba94bd09c5b3f1264f8613f2782f80b6b074c6
Xiaowen Zhang <xiaowen1106@gmail.com>
2025-07-24 22:16:57 -0700

    data update

--- docs/js/config.js
+++ docs/js/config.js
@@ -19,7 +19,7 @@ const getMapboxToken = () => {
 
 export const config = {
     // Version number
-    version: 'v1.0.5',
+    version: 'v1.0.6',
     
     // Load Mapbox token based on current URL
     mapboxToken: getMapboxToken(),
